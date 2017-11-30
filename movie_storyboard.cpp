79674dd62a98e62256dd67106028dcf55f7d8b51
Korinin38 <artkash@bk.ru>
2017-11-30 19:37:10 +0500

    Update movie_storyboard.cpp

--- movie_storyboard.cpp
+++ movie_storyboard.cpp
@@ -23,12 +23,13 @@
 
     void LogoPS(bool stage)
     {
+        int i;
         if (stage==true)
         {
             txSetColor(TX_WHITE);
             txSetFillColor(TX_WHITE);
 
-            for (int i=-4; i<17; i++)
+            for (i=-4; i<17; i++)
             {
                 txSetColor(TX_WHITE);
                 txSetFillColor(TX_WHITE);
@@ -79,5 +80,47 @@
                 }
                 txSleep(100);
             }
+        }
+        else
+        {
+            txSetColor(TX_WHITE, 3);
+            txSetFillColor(TX_BLACK);
+            txClear();
+            txSetFillColor(TX_WHITE);
+            txLine(500, 299, 500, 301);
+            for (i=2; i<128;)
+            {
+                txLine(500, 301-i, 500, 299+i);
+                i+=i-1;
+                txSleep(60);
+            }
+            txLine(500, 159, 500, 441);
+            txSleep(60);
+            txLine(500, 153, 500, 447);
+            txSleep(60);
+            txLine(500, 150, 500, 450);
+            txSleep(400);
+            int ugol;
+            txPie(350, 150, 650, 450, 90, 1);
+            txPie(350, 150, 650, 450, 270, 1);
+            for (ugol=2; ugol<=129;)
+            {
+                txPie(351, 151, 649, 449, 90, ugol);
+                txPie(351, 151, 649, 449, 270, ugol);
+                ugol+=ugol-1;
+                txSleep(60);
+            }
+            for (ugol=176; ugol<180;)
+            {
+                txPie(351, 151, 649, 449, 90, ugol);
+                txPie(351, 151, 649, 449, 270, ugol);
+                ugol+=(180-ugol)/1.7;
+                if (ugol>175) ugol=180;
+                txSleep(60);
+            }
+
+
+            txSetFillColor(TX_BLACK);
+            txClear();
         }
     }
